6144e83498862d0d634c7b9b7ded5e3fc9860f58
davinash97 <avinashd2904@gmail.com>
2026-06-16 11:05:05 +0530

    remove best seller and other useless stuff

--- ease-of-access-pdp.user.js
+++ ease-of-access-pdp.user.js
@@ -4,7 +4,6 @@
 // @version      1.0
 // @description  Copy product title, description, and suggested nodes with the key 'a'
 // @author       avidunna@
-// @match        https://www.amazon.*/*
 // @match        https://www.amazon.co.jp/*
 // @match        https://www.amazon.com/*
 // @match        https://www.amazon.ca/*
@@ -39,14 +38,21 @@
 
 (async () => {
   console.log("ease-of-access: start");
+  document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('[data-expanded="false"]').forEach((e) => {
+      if (e)
+        e.style.display = 'block'
+    });
+  })
+
   async function copyDetails() {
     const getText = (selector) =>
-      document.querySelector(selector)?.innerText.trim() || "";
+      document.querySelector(selector)?.outerText.trim() || "";
 
     return {
       title: getText('#productTitle'),
-      bullets: getText('#feature-bullets') || getText(".a-unordered-list.a-vertical.a-spacing-small"),
-      description:  getText("#voyagerAccordian_feature_div") || getText("#productDescription") || getText('#productDetails_expanderSectionTables') || getText('.a-normal.a-spacing-micro') || getText("#voyager-ns-desktop-side-sheet-container") || getText(".a-row.a-spacing-top-base") || getText("[role='list']"),
+      bullets: getText('#feature-bullets'),
+      description: getText("#productDescription"),
       information: getText("#prodDetails"),
       path: getText('.a-unordered-list.a-horizontal.a-size-small')
     };
@@ -57,33 +63,54 @@
   }
 
   async function pasteAllTheData() {
-      const { title, bullets, description, path, information } = await copyDetails();
+    const { title, bullets, description, path, information } = await copyDetails();
 
-      let result = "";
+    let result = "";
 
-      if(title) {
-        result += `Product Title: ${title}\n\n`;
-      }
+    if (title) {
+      result += `Product Title: ${title}\n\n`;
+    }
 
-      if(bullets) {
-        result += "Product Bullet: " + bullets.trim() + "\n\n";
-      }
+    if (bullets) {
+      result += "Product Bullet: " + bullets.trim() + "\n\n";
+    }
 
-      if(description) {
-        result += `Product Description: ${description.trim().replaceAll(/(Show more|About this item|›\s*See more product details|Product specifications|ASIN[\s\S]*?stars)/gi,"").replaceAll(/\s{2,}/g, " ").replaceAll(/(\(\d+\)\s*\d+(\.\d+)?\s*out of 5 stars)/g,"")}\n\n`;
-      }
+    if (description) {
+      result += `${description.trim().replaceAll(/(Show more|About this item|›\s*See more product details|Product specifications|ASIN[\s\S]*?stars)/gi, "").replaceAll(/\s{2,}/g, " ").replaceAll(/(\(\d+\)\s*\d+(\.\d+)?\s*out of 5 stars)/g, "")}\n\n`;
+    }
 
-      if(information) {
-        result += `${information}\n\n`;
-      }
+    if (information) {
+      document.querySelectorAll('tr').forEach(async (el) => {
+        // console.log(e.children[0].outerText); // 0 is key, 1 is value
+        if (el.children.length >= 2) {
+          let key = el.children[0].innerText || el.children[0].textContent;
+          let value = el.children[1].innerText || el.children[1].textContent;
 
-      if(path) {
-        result += "Suggested Path: " + path.replaceAll(/(copy)/g,"").replaceAll(/(\n)?(\d)?/g, "").replaceAll(/›/g, " $& ");
-      }
+          if (key === 'Best Sellers Rank' || key === 'Customer Reviews' || key === 'Feedback' || key === 'Warranty') return;
 
-      await pasteToCliboard(result);
+          if (key && value) {
+            result += `${key.trim()}: ${value.trim()}\n`; // Added newlines for readability
+          }
+        }
+      })
+    };
 
-      console.log("Copied product details!");
+    // if (information) {
+    //   // result += `${information}\n\n`;
+    //   document.querySelectorAll('#prodDetails > .a-row > .a-column.a-span-last > #productDetails_expanderSectionTables > .a-row > *').forEach((e) => {
+    //     if (e.outerText.includes('Best Sellers Rank')) return;
+    //     // console.log(e);
+    //     result += `${e.outerText.trim()}\n\n`;
+    //   });
+    // }
+
+    if (path) {
+      result += "\nSuggested Path: " + path.replaceAll(/(copy)/g, "").replaceAll(/(\n)?(\d)?/g, "").replaceAll(/›/g, " $& ");
+    }
+
+    await pasteToCliboard(result);
+
+    console.log("Copied product details!");
   }
 
   document.addEventListener('keydown', async (event) => {
@@ -92,16 +119,16 @@
 
     const active = document.activeElement;
 
-		// Allow typing in Amazon editors/inputs
-		if (
-			active.tagName === "INPUT" ||
-			active.tagName === "TEXTAREA" ||
-			active.isContentEditable ||
-			active.closest('[role="textbox"]') ||
-			active.closest('[contenteditable="true"]')
-		) {
-			return;
-		}
+    // Allow typing in Amazon editors/inputs
+    if (
+      active.tagName === "INPUT" ||
+      active.tagName === "TEXTAREA" ||
+      active.isContentEditable ||
+      active.closest('[role="textbox"]') ||
+      active.closest('[contenteditable="true"]')
+    ) {
+      return;
+    }
 
     const key = event.key.toLowerCase();
 
@@ -121,6 +148,6 @@
         break;
     }
   })
-  console.log("ease-of-access: end");
+  console.log("ease-of-access: end")
 }
 )()
